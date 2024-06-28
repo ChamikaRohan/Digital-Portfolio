1e8f7a05352df15f49733f3e8ddc0e6708006c8e
ChamikaRohan <chamika707@gmail.com>
2024-06-28 12:49:39 +0530

    Added asnimations to the parallax pages

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,17 +10,20 @@ export default function Parallax({type}) {
     offset: ["start start", "end start"]
   });
 
+  const yText = useTransform(scrollYProgress, [0,1], ["0%", "200%"]);
   const yBg = useTransform(scrollYProgress, [0,1], ["0%", "100%"]);
 
   return (
-    <div className='parallax' style={{ background:type === "services" 
+    <div ref={ref} className='parallax' style={{ background:type === "services" 
       ? "Linear-gradient(180deg, #111132, #0c0c1d)"
       : "Linear-gradient(180deg, #111132, #505064)" 
     }}>
-        <motion.h1 style={{ y: yBg }}>{type=="services" ? "What We Do?" : "What We Did?"}</motion.h1>
+        <motion.h1 style={{ y: yText }}>{type=="services" ? "What We Do?" : "What We Did?"}</motion.h1>
         <motion.div className="mountains"></motion.div>
-        <motion.div className="planets"></motion.div>
-        <motion.div className="stars"></motion.div>
+        <motion.div className="planets" style={{  y: yBg,
+          backgroundImage: `url(${type === "services" ? "/planets.png":"/sun.png"})`
+        }}></motion.div>
+        <motion.div style={{ x: yBg}} className="stars"></motion.div>
     </div>
   )
 }
