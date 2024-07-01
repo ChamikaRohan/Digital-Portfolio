e6a306c55c7fbed781854db903adc3901e7909af
ChamikaRohan <chamika707@gmail.com>
2024-07-01 09:23:41 +0530

    Initiated portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero.jsx";
 import Parallax from "./components/parallax/Parallax.jsx";
 import About from "./components/about/About.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
 
 const App = () => {
   return <div>
@@ -16,9 +17,7 @@ const App = () => {
     <section><About/></section>
     
     <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <section><Portfolio/></section>
     
     <section id="Contact">Contact</section>
   </div>;
